2f0cf2c4d9d69748ef34c30d21c2c22edfdcd47a
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-16 03:42:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,15 +1,10 @@
 def roll_call_dwarves(dwarves)
-  puts dwarves
-end
-def roll_call_dwarves(array)
   i = 0 
-  while i < array.length 
-    puts "#{i + 1}. #{array[i]}"
+  while i < dwarves.length
+    puts "#{i + 1}, #{dwarves[i]}"
     i += 1 
-  end 
+  end
 end
-
-roll_call_dwarves(dwarves)
  
 
 def summon_captain_planet# code an argument here
